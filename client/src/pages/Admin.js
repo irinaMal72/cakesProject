46157c4a3f7691a6bf793ac063cf63e5d0b90e87
kenzh <kenzhetaevaa@mail.ru>
2021-05-02 11:31:49 +0500

    развернула реакт приложение для фронта черех команду, добавила страницы в папке pages

---
+++ client/src/pages/Admin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Admin = () => {
+    return (
+        < div >
+            AdminPage
+        </div>
+
+    );
+};
+
+export default Admin;
---
+++ client/src/pages/CakePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CakePage = () => {
+    return (
+        < div >
+           CakePage
+        </div>
+
+    );
+};
+
+export default CakePage;
