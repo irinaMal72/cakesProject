8a34f1e96327431eb5ce02bdfac5394d88aabed3
kenzh <kenzhetaevaa@mail.ru>
2021-05-02 11:50:52 +0500

    описание маршрутов для перехода по страницам

---
+++ client/src/routes.js
@@ -0,0 +1,35 @@
+import {ADMIN_ROUTE, LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE} from "./utils/consts";
+import Shop from "./pages/Shop";
+import Auth from "./pages/Auth";
+import CakePage from "./pages/CakePage";
+
+export const authRoutes = [
+    {
+        path: ADMIN_ROUTE,
+        Component: Admin
+    },
+    {
+        path: BASKET_ROUTE,
+        Component: Basket
+    },
+
+] //список маршрутов для тех страниц, которые имеет доступ авторизованный пользователь
+
+export const publicRoutes =[
+    {
+        path: SHOP_ROUTE,
+        Component: Shop
+    },
+    {
+        path: LOGIN_ROUTE,
+        Component: Auth
+    },
+    {
+        path: REGISTRATION_ROUTE,
+        Component: Auth
+    },
+    {
+        path: CAKE_ROUTE+'/:id',
+        Component: CakePage
+    },
+]//список маршрутов для всех пользователей
